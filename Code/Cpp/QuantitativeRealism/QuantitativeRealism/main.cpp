016603eb1f146f6fcdbb1908e1633d46124f2233
Michael Poulshock <mpoulshock@gmail.com>
2021-01-05 17:21:25 -0500

    Added MTCS to IQR.nb; C++ hello world

---
+++ Code/Cpp/QuantitativeRealism/QuantitativeRealism/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  QuantitativeRealism
+//
+//  Created by Michael Poulshock on 1/5/21.
+//
+
+#include <iostream>
+using namespace std;
+
+string f() {
+    return "Hello, world";
+}
+
+int main(int argc, const char * argv[]) {
+    
+    std::cout << f() + "\n"; 
+    return 0;
+}
+
+
